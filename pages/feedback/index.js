e0597e92f90ff8dcca72565edbd6f2f11bdfba82
peng <peng@qq.com>
2020-05-11 22:11:55 +0800

    反馈界面

--- pages/feedback/index.js
+++ pages/feedback/index.js
@@ -1,66 +1,35 @@
 // pages/feedback/index.js
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-
+    tabs: [
+      {
+        id: 0,
+        tabName: "体验问题",
+        isActive: true,
+      },
+      {
+        id: 1,
+        tabName: "商品、商家投诉",
+        isActive: false,
+      },
+    ],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function (options) {},
 
+  handleTabClick(e) {
+    let { index } = e.detail;
+    let tabs = this.data.tabs;
+    tabs.forEach((item, i) => (i === index ? (item.isActive = true) : (item.isActive = false)));
+
+    this.setData({
+      tabs,
+    });
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
-})
+});
--- pages/search/index.js
+++ pages/search/index.js
@@ -4,17 +4,24 @@
  *    2. 合法性判断
  *    3. 检验通过 把输入框的值 发送到后台
  *    4. 返回的数据打印到页面上
+ *
+ * 2. 微信只能单向绑定，从JS -> HTML ,所以设置了2个inputValue值
+ *      inputValue为与value绑定的数据，用来在点击取消按钮之后，清空数据
+ *      inpValue 是 用来传输给后台的
  */
 
 import { request } from "../../request/index.js";
 Page({
   data: {
     goods: [],
+    isHidden: true,
+    inputValue: "",
   },
+  inpValue: "",
   pageNum: 1,
-  inputValue: "",
   trueFunc: null,
   total: 0,
+
   onLoad() {
     // 防抖外边只执行一次，里面的根据输入执行
     this.trueFunc = this.deBounce((value, count) => {
@@ -24,18 +31,25 @@ Page({
   handleInput(e) {
     let { value } = e.detail;
 
-    this.inputValue = value.trim();
-    //每次输入时清空先前的数组
+    this.inpValue = value.trim();
+    //每次输入时清空先前的数组，重置其他数
     this.setData({
       goods: [],
     });
     this.pageNum = 1;
     this.total = 0;
-    if (this.inputValue != "") {
-      this.trueFunc(this.inputValue, this.pageNum);
+
+    if (this.data.isHidden) {
+      this.setData({
+        isHidden: false,
+      });
+    }
+    if (this.inpValue.length != 0) {
+      this.trueFunc(this.inpValue, this.pageNum);
     }
   },
 
+  //发送请求
   getSearch(value, count) {
     request({
       url: `goods/search`,
@@ -56,14 +70,27 @@ Page({
     });
   },
 
+  //下拉时候，继续加载
   onReachBottom() {
     //在第一次发送请求之后，会获取total总量，然后与 goods的长度比较。
     if (this.total > this.data.goods.length) {
       ++this.pageNum;
-      this.trueFunc(this.inputValue, this.pageNum);
+      this.trueFunc(this.inpValue, this.pageNum);
     }
   },
 
+  //点击取消按钮 （输入框清空）
+  handleCancle() {
+    this.setData({
+      goods: [],
+      inputValue: "",
+      isHidden: true,
+    });
+  },
+
+  /**
+   * 防抖函数
+   */
   deBounce(fn, delay) {
     let timer = null;
 
